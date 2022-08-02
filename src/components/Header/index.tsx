d09f71662d1b2fda736f8e7e872c7ad33fc35b4d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-08-02 13:08:23 -0300

    feat: import header in _app.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
+import styles from "./styles.module.scss";
+
 export function Header() {
   return (
-    <header>
-      <div>
-        <img src="/images/logo.svg" alt="Logo do IGNews" />
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Logo do ig.news" />
         <nav>
           <a>Home</a>
           <a>Posts</a>
