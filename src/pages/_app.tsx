d990c043beced65c7d89af1fe4439d6deda3d991
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-08-04 18:19:39 -0300

    feat: auth with nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider } from "next-auth/react";
 import { Header } from "../components/Header";
 import { AppProps } from "next/app";
 import "../styles/global.scss";
@@ -5,8 +6,10 @@ import "../styles/global.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header/>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
