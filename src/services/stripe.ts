28052a1851b89eae094aba273500c53d766a2d2c
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-09-23 13:09:55 -0300

    fix: warn version

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from "stripe";
-import {version} from '../../package.json';
+import packageInfo from '../../package.json'
+const { version } = packageInfo
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2022-08-01",
