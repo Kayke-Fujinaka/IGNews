d607fa7e4c62594806ec014f15d4983a47262f70
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-25 18:21:00 -0300

    feat: improve acessibility and SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="description"
+            content="A Blog with integrated CMS and payment method with subscription"
+          />
+          <meta name="author" content="Kayke Fujinaka" />
         </Head>
         <body>
           <Main />
