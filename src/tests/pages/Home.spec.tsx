3ba32f6fad61b8a654d07d5e97e959cfee054bd9
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-25 18:22:33 -0300

    test: Home page

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../../pages";
+
+jest.mock("next-auth/react", () => ({
+  useSession: () => [null, false],
+}));
+
+describe("Home page", () => {
+  it("should be able to renders correctly", () => {
+    render(
+      <Home
+        product={{
+          priceId: "fake-price-id",
+          amount: "R$10,00",
+        }}
+      />
+    );
+
+    expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+});
