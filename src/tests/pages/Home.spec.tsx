43289bf7ab19c63b849b00325ef63baf06669ed8
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-25 18:27:54 -0300

    test: getStaticProps from Home page

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../../pages";
+import Home, { getStaticProps } from "../../pages";
+import { stripe } from "../../services/stripe";
 
 jest.mock("next-auth/react", () => ({
   useSession: () => [null, false],
 }));
+
+jest.mock("../../services/stripe");
 
 describe("Home page", () => {
   it("should be able to renders correctly", () => {
@@ -17,5 +20,21 @@ describe("Home page", () => {
     );
 
     expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+
+  it("should be loads init data", async () => {
+    const stripedMocked = jest.mocked(stripe.prices.retrieve);
+    stripedMocked.mockResolvedValueOnce({
+      id: "fake-price-id",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: { product: { priceId: "fake-price-id", amount: "$10.00" } },
+      })
+    );
   });
 });
